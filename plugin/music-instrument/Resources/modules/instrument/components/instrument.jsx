17eb02f4c5d8aee113fe07c0294012b71b285483
Elorfin <penin.axel@gmail.com>
2016-12-29 09:05:31 +0100

    test midi api

--- plugin/music-instrument/Resources/modules/instrument/components/instrument.jsx
+++ plugin/music-instrument/Resources/modules/instrument/components/instrument.jsx
@@ -32,6 +32,21 @@ const node = {
 }
 
 class Instrument extends Component {
+  componentDidMount() {
+    this.midi = null  // global MIDIAccess object
+
+    function onMIDISuccess( midiAccess ) {
+      console.log( "MIDI ready!" )
+      this.midi = midiAccess  // store in the global (in real usage, would probably keep in an object instance)
+    }
+
+    function onMIDIFailure(msg) {
+      console.log( "Failed to get MIDI access - " + msg )
+    }
+
+    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure)
+  }
+
   render() {
     return (
       <div>
