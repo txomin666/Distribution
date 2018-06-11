c678cec0d618bbbd81658f7af44a761bfe380203
TANDJIEKPON <judicaeltandjiekpon@yahoo.fr>
2018-06-11 14:39:23 +0200

    Merge, Add region, Play Region, List Region

--- plugin/audio-react/Resources/modules/resources/audio/player/waveplayer.jsx
+++ plugin/audio-react/Resources/modules/resources/audio/player/waveplayer.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import {actions} from '#/plugin/audio-react/resources/audio/actions'
 import {connect} from 'react-redux'
-<<<<<<< HEAD
 import {select as resourceSelect} from '#/main/core/resource/selectors'
 import WaveSurferControl from '#/plugin/audio-react/resources/audio/player/components/wavesurfer-control'
 import WaveSurferRegionList from '#/plugin/audio-react/resources/audio/player/components/wavesurfer-list-region'
-=======
->>>>>>> 29f8b47be55c5e7e6931963a02715d88bbd3ac4d
+
 import WaveSurfer from 'wavesurfer.js/dist/wavesurfer'
 import 'wavesurfer.js/dist/plugin/wavesurfer.minimap.min'
 import 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min'
 import 'wavesurfer.js/dist/plugin/wavesurfer.regions.min'
 import {PropTypes as T} from 'prop-types'
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 29f8b47be55c5e7e6931963a02715d88bbd3ac4d
 /**
  * Simple example of a React component with a Wavesurfer
  */
@@ -30,7 +24,6 @@ class WavePlayer extends React.Component {
       pos: 0,
       volume: 0.5,
       audioRate: 1,
-<<<<<<< HEAD
       regionActive: null
     }
     this.ToogglePlay = this.ToogglePlay.bind(this)
@@ -38,23 +31,14 @@ class WavePlayer extends React.Component {
     this.regionPlay = this.regionPlay.bind(this)
     this.regionReset = this.regionReset.bind(this)
     this.regionActiveUpdate = this.regionActiveUpdate.bind(this)
-=======
-      regionActive:null
-    }
-    this.ToogglePlay= this.ToogglePlay.bind(this)
-    this.regionAdd= this.regionAdd.bind(this)
-    this.regionPlay = this.regionPlay.bind(this)
->>>>>>> 29f8b47be55c5e7e6931963a02715d88bbd3ac4d
+
   }
 
   uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-<<<<<<< HEAD
+
       var r = (Math.random() * 16) | 0,
         v = c == 'x' ? r : (r & 0x3) | 0x8
-=======
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
->>>>>>> 29f8b47be55c5e7e6931963a02715d88bbd3ac4d
       return v.toString(16)
     })
   }
@@ -70,7 +54,6 @@ class WavePlayer extends React.Component {
     this.wavesurfer.load(this.props.src)
 
     /** Clic on region */
-<<<<<<< HEAD
     this.wavesurfer.on('region-click', r => {
       this.setState({regionActive: r})
     })
@@ -139,85 +122,13 @@ class WavePlayer extends React.Component {
   }
 
   regionRemove() {
-=======
-    this.wavesurfer.on('region-click',(r)=>{
-      this.setState({regionActive:r})
-    }) 
 
-    /** Created on region */
-    this.wavesurfer.on('region-created',(r)=>{
-
-
-    }) 
-
-    /** Deleted on region */
-    this.wavesurfer.on('region-delected',(r)=>{
-
-    }) 
-
-    /** Deleted on region */
-    this.wavesurfer.on('region-dblclick',(r,e)=>{
-      console.log(r.end)
-      this.wavesurfer.play(r.start,r.end)
-    
-    }) 
-
-    /** Initialize First Region on WaveSurfer */
-    this.wavesurfer.on('ready',()=>{
-      let r = {id:this.uuidv4(),start:0,end:this.wavesurfer.getDuration()-0.01}
-      this.wavesurfer.addRegion(r)
-      this.setState({regionActive:r})
-    })  
-    
-  } 
-
-
-
-  /** Play - Pause  */
-  ToogglePlay(){
-    this.wavesurfer.playPause()
-    if(this.state.playing)
-      this.setState({playing:false})
-    else 
-      this.setState({playing:true})
-  }
-
-
-
-  regionAdd(){
-    if(this.state.regionActive){
-      delete this.wavesurfer.regions.list[this.state.regionActive.id]
-      let first = {
-        id:this.uuidv4(),
-        start:this.state.regionActive.start,
-        end:this.wavesurfer.getCurrentTime(),
-        color:'rgba(14, 90, 60, 0.4)'
-        
-      }  
-
-      let second = {
-        id:this.uuidv4(),
-        start:this.wavesurfer.getCurrentTime()+0.01,
-        end:this.state.regionActive.end,
-        color:'rgba(56, 90, 100, 0.4)'
-
-
-      }
-      this.wavesurfer.addRegion(first)
-      this.wavesurfer.addRegion(second)
-        
-    }
-  }
-
-  regionRemove(){
->>>>>>> 29f8b47be55c5e7e6931963a02715d88bbd3ac4d
     // regionList.forEach((region,index,object) => {
     //   if(region.id == this.state.wavesurfer.regionActive.id)
     //     object.splice(index, 1)
     // })
   }
 
-<<<<<<< HEAD
   regionActiveUpdate(r){
     //console.log(r)
   }
@@ -289,33 +200,12 @@ class WavePlayer extends React.Component {
           </div>
         </div>
       )
-=======
-  regionPlay(){
-    this.wavesurfer.play(this.state.regionActive.start,this.state.regionActive.end)
-  }
 
-  render() {
-    return (
-      <div>
-        <div className='player-react'>
-          <div className='wave'></div>
-        </div>
-        <div className="row ">
-          <div className="col-xs-4 col-md-offset-4 player-react-control">
-            <button className="btn btn-primary " onClick={this.ToogglePlay}> <span className={`fa big ${this.state.playing ? 'fa-pause' : 'fa-play'}`}></span></button>
-            <button className="btn btn-success " onClick={this.regionAdd}> <span className='fa fa-flag'></span></button>
-            <button className="btn btn-primary " onClick={this.regionPlay}> <span className='fa fa-repeat'></span></button>
-          </div>
-        </div>
-      </div>
 
-    )
->>>>>>> 29f8b47be55c5e7e6931963a02715d88bbd3ac4d
   }
 }
 
 WavePlayer.propTypes = {
-<<<<<<< HEAD
   src: T.string.isRequired,
   edit: T.bool.isRequired,
   updateRegions:T.func.isRequired,
@@ -333,16 +223,3 @@ export default connect(
     updateActiveRegion:region =>dispatch(actions.updateRegionActivePlayer(region))
   })
 )(WavePlayer)
-=======
-  src: T.string.isRequired
-}
-
-export default connect(
-  state=>({
-    url: state.url
-  }), 
-  (dispatch)=>({
-    addRegion :(region)=>dispatch(actions.addRegionToPlayer(region))
-  }))(WavePlayer)
-
->>>>>>> 29f8b47be55c5e7e6931963a02715d88bbd3ac4d
