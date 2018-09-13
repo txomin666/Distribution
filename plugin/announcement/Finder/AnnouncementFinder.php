2b1f34337e5f86bc4b40d296a819df93a1407102
ngodfraind <nicolas.godfraind@gmail.com>
2018-09-13 17:55:04 +0200

    Announcement widget filters for ws;

--- plugin/announcement/Finder/AnnouncementFinder.php
+++ plugin/announcement/Finder/AnnouncementFinder.php
@@ -11,6 +11,7 @@
 
 namespace Claroline\AnnouncementBundle\Finder;
 
+use Claroline\AnnouncementBundle\Entity\Announcement;
 use Claroline\AppBundle\API\Finder\AbstractFinder;
 use Doctrine\ORM\QueryBuilder;
 use JMS\DiExtraBundle\Annotation as DI;
@@ -23,7 +24,7 @@ class AnnouncementFinder extends AbstractFinder
 {
     public function getClass()
     {
-        return 'Claroline\AnnouncementBundle\Entity\Announcement';
+        return Announcement::class;
     }
 
     public function configureQueryBuilder(QueryBuilder $qb, array $searches = [], array $sortBy = null)
@@ -67,6 +68,24 @@ class AnnouncementFinder extends AbstractFinder
                       $qb->expr()->andX(...$expr);
                       $qb->andWhere('obj.visible = true');
                   }
+                  break;
+                case 'user':
+                  $qb->leftJoin('obj.aggregate', 'a');
+                  $qb->leftJoin('a.resourceNode', 'node');
+                  $qb->leftJoin('node.workspace', 'w');
+                  $qb->leftJoin('w.roles', 'r');
+                  $qb->leftJoin('r.users', 'ru');
+                  $qb->leftJoin('r.groups', 'rg');
+                  $qb->leftJoin('rg.users', 'rgu');
+                  $qb->andWhere($qb->expr()->orX(
+                      $qb->expr()->eq('ru.uuid', ':currentUserId'),
+                      $qb->expr()->eq('rgu.uuid', ':currentUserId'),
+                      $qb->expr()->eq('ru.id', ':currentUserId'),
+                      $qb->expr()->eq('rgu.id', ':currentUserId')
+                  ));
+                  $qb->andWhere('r.name != :roleUser');
+                  $qb->setParameter('currentUserId', $filterValue);
+                  $qb->setParameter('roleUser', 'ROLE_USER');
                   break;
                 default:
                     $this->setDefaults($qb, $filterName, $filterValue);
